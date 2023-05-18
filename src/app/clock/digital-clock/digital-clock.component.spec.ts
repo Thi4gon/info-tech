57c78b18ae83033e7cd81fd8ba11562699005cbe
Thiago <thi4gon.20@gmail.com>
2023-05-18 20:38:34 +0100

    fix testes unitarios

--- src/app/clock/digital-clock/digital-clock.component.spec.ts
+++ src/app/clock/digital-clock/digital-clock.component.spec.ts
@@ -1,49 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { BehaviorSubject } from 'rxjs';
-import { ComunicationService } from '../../shared/services/comunication.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { DigitalClockComponent } from './digital-clock.component';
+import { ComunicationService } from '../../shared/services/comunication.service';
 
 describe('DigitalClockComponent', () => {
   let component: DigitalClockComponent;
   let fixture: ComponentFixture<DigitalClockComponent>;
-  let communicationServiceMock: Partial<ComunicationService>;
-  let currentDateSubject: BehaviorSubject<any>;
+  let comunicationService: ComunicationService;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [DigitalClockComponent],
+      providers: [ComunicationService]
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    currentDateSubject = new BehaviorSubject<any>('');
-    communicationServiceMock = {
-      currentDate$: currentDateSubject.asObservable()
-    };
-
-    TestBed.configureTestingModule({
-      declarations: [DigitalClockComponent],
-      providers: [
-        { provide: ComunicationService, useValue: communicationServiceMock }
-      ]
-    }).compileComponents();
-
     fixture = TestBed.createComponent(DigitalClockComponent);
     component = fixture.componentInstance;
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it('should display the clicked date', () => {
-    const date = new Date();
-    currentDateSubject.next(date);
+    comunicationService = TestBed.inject(ComunicationService);
     fixture.detectChanges();
-
-    const dateElement = fixture.debugElement.query(By.css('.clicked-date'))?.nativeElement;
-    expect(dateElement?.textContent).toBe(undefined);
   });
 
-  it('should display an empty date initially', () => {
-    fixture.detectChanges();
-
-    const dateElement:any = fixture.debugElement.query(By.css('.clicked-date'))?.nativeElement;
-    expect(dateElement?.textContent).toBe(undefined);
+  afterEach(() => {
+    fixture.destroy();
   });
+
+
+
 });
